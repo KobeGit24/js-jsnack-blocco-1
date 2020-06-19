9f0599320aeb07a1f60b454e80ea16ecc0926b81
Stefano <stefano.zaccaria29@gmail.com>
2020-06-19 16:50:13 +0200

    ex complete!

--- -jsnacks-bonus/script.js
+++ -jsnacks-bonus/script.js
@@ -2,14 +2,19 @@ var btn = document.getElementById('btn');
 
 btn.addEventListener('click', function () {
     var numRandom = Math.floor(Math.random() * 10);
+    var money = document.getElementById('money');
     var frontFace = document.getElementById('front-face');
     var retroFace = document.getElementById('retro-face');
+    money.classList.remove('hidden');
     
 
-    if (numRandom % 2 == 1) {  
+    if (numRandom % 2 == 1) { 
         frontFace.classList.remove('hidden');
+        retroFace.classList.add('hidden');
+
     } else if (numRandom % 2 == 0) {
         retroFace.classList.remove('hidden');
+        frontFace.classList.add('hidden');
     }
 
     console.log(numRandom);
