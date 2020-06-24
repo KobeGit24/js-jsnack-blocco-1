b3970d8c799f0b639a99b588bbaed759a122d52d
Stefano <stefano.zaccaria29@gmail.com>
2020-06-24 17:46:42 +0200

    LC

--- js-livecoding/script.js
+++ js-livecoding/script.js
@@ -34,7 +34,7 @@
 
 //LIVE CODING CICLO WHILE!
 
-var nomi = ["giulio", 'augusto','marco'];
+// var nomi = ["giulio", 'augusto','marco'];
 
 // for (var i = 0; i < nomi.length; i++) {
 
@@ -42,10 +42,75 @@ var nomi = ["giulio", 'augusto','marco'];
 
 // };
 
-var i = 0;
+// var i = 0;
 
-while (i < nomi.length) {
+// while (i < nomi.length) {
 
-    console.log(nomi[i]);
-    i++;
-};
+//     console.log(nomi[i]);
+//     i++;
+// };
+
+// LIVE CODING
+
+// var nomi = ["flavio", 'giovanni','marco','davide'];
+
+// var nonTrovato = true;
+// var i = 0;
+
+// while (nonTrovato && nomi[i] != undefined) {
+//     if (nomi[i++] == 'flavio')
+//     nonTrovato = false;
+// }
+
+//LIVE CODING CICLO DO WHILE
+
+// var numeri = [];
+
+// do {
+//     var num = Math.floor(Math.random()*100)+1;
+//     console.log(num);
+//     numeri.push(num);  
+// }   while (num%2);
+
+// console.log(numeri);
+
+
+// for (var index = 0; index < spesa.length; index++) {
+//     console.log(spesa[index]);  
+// }
+
+// var i = 0;
+
+// while (i<spesa.length) {
+//     console.log(spesa[i]);
+//     i++;
+// }
+
+// LIVE CODING ESERCIZIO
+
+// var surname = ['Rossi', 'Bianco', 'Esposito', 'Russo', 'Zaccaria'];
+
+// var userSurname = prompt("INSERISCI IL TUO COGNOME");
+
+// surname.push(userSurname);
+
+// surname = surname.sort();
+
+// console.log(surname);
+
+// var debug = true;
+
+// var userPosition = surname.indexOf(userSurname);
+
+// console.log(userPosition + 1);
+
+// for (var i = 0; i < surname.length; i++) { 
+    
+//     if (surname[i] == userSurname ) {
+//         console.log(surname[i]);
+//         var debug = false;
+//     } 
+// }
+
+
+
