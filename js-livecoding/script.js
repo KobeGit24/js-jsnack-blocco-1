7aae136a5621b652d66b0bc0a704585f7dd4fe54
Stefano <stefano.zaccaria29@gmail.com>
2020-07-03 16:37:06 +0200

    ok

--- js-livecoding/script.js
+++ js-livecoding/script.js
@@ -347,3 +347,48 @@
 // }
 
 // console.log(operation, num1, num2, '=', result);
+
+// EVENTI JQUERY LEZIONE 03/07
+
+//keypress(); // e tutti gli eventi key!
+// $(document).keydown(function () {
+//     var key = event.which;
+//     if (key == 39 || key == 100) {
+//         nextImg(); 
+//     } else if ( key ==  || key == ) {
+//         prevImg();
+//     }
+// })
+
+// function nextImg() {
+//     var imgActive = $('.slider-wrapper img.active');
+//     imgActive.removeClass('active');
+//     var iActive = $('.slider-wrapper i.active');
+//     iActive.removeClass('active');
+    
+//     if(imgActive.hasClass('last') == true) {
+//         $('.slider-wrapper img.first').addClass('active');  
+//         $('.slider-wrapper i.first').addClass('active');  
+//     } else {
+//         imgActive.next().addClass('active');
+//         iActive.next().addClass('active');
+//     }
+// }
+
+// function prevImg() {
+//     var imgActive = $('.slider-wrapper img.active');
+//     imgActive.removeClass('active');
+//     var iActive = $('.slider-wrapper i.active');
+//     iActive.removeClass('active');
+    
+//     if(imgActive.hasClass('first') == true) {
+//         $('.slider-wrapper img.last').addClass('active');  
+//         $('.slider-wrapper i.last').addClass('active');  
+//     } else {
+//         imgActive.prev().addClass('active');
+//         iActive.prev().addClass('active');
+//     }
+// }
+
+
+
