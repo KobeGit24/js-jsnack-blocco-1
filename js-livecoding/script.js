1d97438399fa8cf6319487e538ef9d34d1331179
Stefano <stefano.zaccaria29@gmail.com>
2020-07-10 11:16:24 +0200

    ok

--- js-livecoding/script.js
+++ js-livecoding/script.js
@@ -412,13 +412,10 @@
 // }
 
 //un alert espone 5 numeri casuali. da li parte un timer di 30 secondi. dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precendentemente. dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuali.
-$(document).ready(function(){
-    var array = [];
-    var user = [];
+// $(document).ready(function(){
+//     var array = [];
+//     var user = [];
     
-    // for (var i = 0; i < 5; i++) {
-    //     array.push(random(1,100));   
-    // }
 //     while (array.length<5) {
 //         var numRan = random(1,100);
 //         if (!array.includes(numRan)) {
@@ -428,25 +425,39 @@ $(document).ready(function(){
 //     console.log(array);
     
 //     var time = 5;
+//     var control = false;
     
 //     var timer = setInterval(function() {  
-//         if (time==0) {
+//         if (time==0 && control==false) {
 //             clearInterval(timer);
 //             $('#modal').removeClass('active').text('');
-//             $('#user-num').addClass('active');
+//             control=true;
+//         }  else if (time==0 && control==true) {   
 //             i=0;
 //             while (i<5) {
-//                 var numUser = $('#user').val();
+//                 var numUser = parseInt(prompt('inserisci un numero da 1 a 100'))
 //                 user.push(numUser);
 //                 i++;
-//                 console.log(user);   
 //             }
-//         } else {
+//             console.log(user);   
+//         }
+//         else {
 //             time--;
 //             $('#modal').addClass('active').text(array);
 //             console.log(time); 
 //         }
 //     },1000);
+
+//     if (time==0 && control==true) {   
+//         i=0;
+//         while (i<5) {
+//             var numUser = parseInt(prompt('inserisci un numero da 1 a 100'))
+//             user.push(numUser);
+//             i++;
+//         }
+//         console.log(user);   
+//     }
+
 // });
 
 
